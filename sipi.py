9caa0ce30091dac0d1094ab4aa0cd4d390aa469e
PrincessSipi <94324191+PrincessSipi@users.noreply.github.com>
2022-01-08 21:23:38 +0000

    10 lines added, program not running(Please help)

--- sipi.py
+++ sipi.py
@@ -1,1 +1,22 @@
 print("hello")
+# I'M GOING TO DEFINE A METHOD
+
+class Children:
+
+    def __init__(self, name , age , height):
+
+        self.name: str = name
+        self.age: int = age
+        self.height: float = height
+        print("My name is" + name + ",I am " + age + ", years old. I am " + height + "metres, tall")
+
+
+c1 = Children("Sammy", 10, 1.2)
+c2 = Children("Charlie", 14, 1.5)
+c3 = Children("Billy", 12, 1.3)
+
+
+
+
+
+
